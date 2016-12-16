be566d65b96c634563c5fa35ea95ee5fe343297f
Édouard Hue <edouard.hue@orange.com>
2016-12-16 17:01:00 +0100

    URLs

--- civicrm.go
+++ civicrm.go
@@ -3,6 +3,8 @@ package main
 import (
 	"net/http"
 	"encoding/json"
+	"fmt"
+	"os"
 )
 
 const url = "https://crm.wikimedia.fr"
@@ -12,19 +14,18 @@ type searchResponse struct {
 }
 
 func SearchContact(email string) {
-	resp, err := http.DefaultClient.Get(url)
-	if err != nil {
-		return err
+	if resp, err := http.DefaultClient.Get(url); err != nil {
+		fmt.Fprintln(os.Stderr, "Error contacting CiviCRM", err)
+	} else {
+		defer resp.Body.Close()
+		decoder := json.NewDecoder(resp.Body)
+		var response searchResponse
+		if err := decoder.Decode(&response); err != nil {
+			fmt.Fprintln(os.Stderr, "Could not parse CiviCRM response", err)
+		} else {
+			return response
+		}
 	}
-	defer resp.Body.Close()
-
-	decoder := json.NewDecoder(resp.Body)
-	var response searchResponse
-	err = decoder.Decode(&response)
-	if err != nil {
-		return err
-	}
-	return response
 }
 
 func ListContactMemberships(contact string) {

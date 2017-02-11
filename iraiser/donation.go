4c201ef5b9554b12730fa7b142e1eb6764c7ec98
Édouard Hue <edouard.hue@gmail.com>
2017-02-11 22:05:14 +0100

    Adaptation du binding json

--- iraiser/donation.go
+++ iraiser/donation.go
@@ -5,32 +5,32 @@ import (
 )
 
 type Donation struct {
-	Donator        Donator `json:"donator" binding:"required"`
-	Campaign       Campaign `json:"campaign" binding:"required"`
-	Payment        Payment `json:"payment" binding:"required"`
-	Amount         int `json:"amount" binding:"required"`
+	Donator
+	Campaign
+	Payment
+	Amount         int `json:"amount,string" binding:"required"`
 	Currency       string `json:"currency" binding:"required"`
 	Reference      string `json:"reference" binding:"required"`
 	ValidationDate time.Time `json:"validationDate" binding:"required"`
 }
 
 type Donator struct {
-	Mail          string `json:"email" binding:"required"`
-	FirstName     string `json:"firstName" binding:"required"`
-	LastName      string `json:"lastName" binding:"required"`
-	Pseudo        string `json:"pseudo" binding:"required"`
-	StreetAddress string `json:"address" binding:"required"`
-	City          string `json:"city" binding:"required"`
-	PostalCode    string `json:"postcode" binding:"required"`
-	Country       string `json:"country" binding:"required"`
+	Mail          string `json:"donator.email" binding:"required"`
+	FirstName     string `json:"donator.firstName" binding:"required"`
+	LastName      string `json:"donator.lastName" binding:"required"`
+	Pseudo        string `json:"donator.pseudo" binding:"required"`
+	StreetAddress string `json:"donator.address" binding:"required"`
+	City          string `json:"donator.city" binding:"required"`
+	PostalCode    string `json:"donator.postcode" binding:"required"`
+	Country       string `json:"donator.country" binding:"required"`
 }
 
 type Campaign struct {
-	AffectationCode string `json:"affectationCode" binding:"required"`
-	OriginCode      string `json:"originCode" binding:"required"`
+	AffectationCode string `json:"campaign.affectationCode" binding:"required"`
+	OriginCode      string `json:"campaign.originCode" binding:"required"`
 }
 
 type Payment struct {
-	Mode      string `json:"mode" binding:"required"`
-	GatewayId string `json:"gatewayId" binding:"required"`
+	Mode      string `json:"payment.mode" binding:"required"`
+	GatewayId string `json:"payment.gatewayId" binding:"required"`
 }
--- iraiser/donation_test.go
+++ iraiser/donation_test.go
@@ -4,15 +4,34 @@ import (
 	"testing"
 	"encoding/json"
 	"io/ioutil"
+	"github.com/stretchr/testify/assert"
+	"time"
 )
 
 func TestDonationJsonBinding(t *testing.T) {
-	if bytes, err := ioutil.ReadFile("sample_donation.json"); err != nil {
+	bytes, err := ioutil.ReadFile("sample_donation.json")
+	if err != nil {
 		t.Fatal(err)
-	} else {
-		donation := Donation{}
-		if err := json.Unmarshal(bytes, &donation); err != nil {
-			t.Fatal(err)
-		}
 	}
+	donation := []Donation{}
+	if err := json.Unmarshal(bytes, &donation); err != nil {
+		t.Fatal(err)
+	}
+	assert.NotEmpty(t, donation)
+	assert.Equal(t, "john@doe.fr", donation[0].Donator.Mail)
+	assert.Equal(t, "John-Jérémy", donation[0].Donator.FirstName)
+	assert.Equal(t, "Doe", donation[0].Donator.LastName)
+	assert.Equal(t, "Jdoe", donation[0].Donator.Pseudo)
+	assert.Equal(t, "25 rue du port", donation[0].Donator.StreetAddress)
+	assert.Equal(t, "Léon", donation[0].Donator.City)
+	assert.Equal(t, "27400", donation[0].Donator.PostalCode)
+	assert.Equal(t, "FR", donation[0].Donator.Country)
+	assert.Equal(t, "", donation[0].Campaign.AffectationCode)
+	assert.Equal(t, "", donation[0].Campaign.OriginCode)
+	assert.Equal(t, "card", donation[0].Payment.Mode)
+	assert.Equal(t, "9533016784", donation[0].Payment.GatewayId)
+	assert.Equal(t, 1200, donation[0].Amount)
+	assert.Equal(t, "EUR", donation[0].Currency)
+	assert.Equal(t, "WIKI20170210-1298-1387", donation[0].Reference)
+	assert.Equal(t, time.Date(2016, time.October, 28, 14, 18, 43, 0, time.Local), donation[0].ValidationDate)
 }
